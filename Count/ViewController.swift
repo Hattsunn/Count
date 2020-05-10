1b747215817a586069133dde6917760ab102d30c
okada hatsune <swan.nilda1211@gmail.com>
2020-05-10 10:12:14 +0900

    たす、引く、わる、かけるカウントアプリの完成

--- Count/ViewController.swift
+++ Count/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+    var number: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func plus() {
+        number = number + 1 // 今までのnumberに１プラスする
+        label.text = String(number)
 }
+
+    @IBAction func minus() {
+        number = number - 1
+        label.text = String(number)
+    }
+       
+    @IBAction func division() {
+        number = number / 2
+        label.text = String(number)
+    }
+    
+    @IBAction func multiplication() {
+        number = number * 2
+        label.text = String(number)
+    }
+    
+       @IBAction func clear() {
+           number = 0
+           label.text = String(number)
+       }
+       
+   }
 
